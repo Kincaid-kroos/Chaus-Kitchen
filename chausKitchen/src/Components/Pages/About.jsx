342262743ebc758c419631a4079d82b5a7eb96cf
kincaid <kiprotichkincaid@gmail.com>
2023-12-16 09:59:59 +0300

    made some changes

--- chausKitchen/src/Components/Pages/About.jsx
+++ chausKitchen/src/Components/Pages/About.jsx
@@ -6,34 +6,40 @@ const About = () => {
   return (
     <div id="about" className='mt-10 py-6 px-16'>
       {/*flex items*/}
-      <div className="flex flex-col-reverse md:flex-row  items-center sm:items-start mx-auto mt-0 md:mt-10  md:space-x-16 space-x-10 space-y-2 md:space-y-0 ">
+      <div className="flex flex-col md:flex-row  items-center sm:items-start mx-auto mt-0 md:mt-10  md:space-x-16 space-x-10 space-y-2 md:space-y-0 ">
         {/*item a*/}
         <div className=" flex flex-col md:w-1/2"> 
-        <img  src={about} alt="about img" className="md:h-[400px] md:w-[400px] md:rounded-full rounded-tr-3xl rounded-bl-3xl"/>
+        <img  src={about} alt="about img" className="md:h-[400px] md:w-[400px] rounded-full object-cover"/>
 
         </div>
          {/*item b*/}
          <div className='flex flex-col md:w-1/2 mt-2 sm:items-start'>
-         <h2 className="font-bold text-4xl text-center md:text-left  md:text-7xl text-[#7ab916]">
+         <h2 className="font-bold text-4xl  text-left  md:text-7xl text-[#7ab916]">
             About Us
           </h2>
           
-          <p className='text-gray-400 my-4 text-sm md:text-xl font-semibold  w-[90%] text-center md:text-left'>
+          <p className='text-gray-400 my-4 text-1xl md:text-xl font-semibold  md:w-[90%]  text-left'>
           This is our Mombasa based Barbaque web app that allows users to book their meals for the week. 
           They can also add recipes and ingredients to their dine list.
           We make amazing food ranging from African to American and everything in between. 
           We have been operating for over 10 years and we love to share our passion with others.
 
-We have services ranging from hospitality, in house and outdoor catering and cooking classes.
- We also do private events and parties.
+        We have services ranging from hospitality, in house and outdoor catering and cooking classes.
+        We also do private events and parties.
 
           </p>
-          <button className=" bg-[#ffa300] flex gap-2 font-normal md:font-bold cursor-pointer animate-bounce
-          rounded-tr-2xl rounded-bl-2xl hover:bg-[#16B97A] transition-all duration-300 text-white
-           md:text-md text-sm py-3  px-6 md:px-8 md:py-4 w-[50%] ">
-              <p>Book Now</p> 
-              <BiPhoneCall className="text-white text-xl" />
-            </button>
+        
+
+            <div className="flex  items-center gap-4 hover:bg-[#ffa300] font-bold 
+            cursor-pointer animate-bounce rounded-tr-2xl rounded-bl-2xl bg-[#16B97A] 
+            transition-all duration-300 text-white
+            md:text-md text-sm py-1  px-12 md:px-12 md:py-1 mt-2">
+              <button>
+              <BiPhoneCall className="text-[#e4ded5] text-xl md:text-5xl cursor-pointer hover:text-[#282752] transition-all duration-300" />
+              </button>
+              <p>Call us Now</p>
+            </div>
+            
           
           </div>
 
--- chausKitchen/src/Components/Pages/Testimonials.jsx
+++ chausKitchen/src/Components/Pages/Testimonials.jsx
@@ -9,16 +9,30 @@ const Testimonials = () => {
     <div className=" px-5 text-center">
     {/*Heading*/} 
     <h2 className="font-bold text-center 
-     md:pt-4  text-4xl   md:text-4xl text-[#7ab916]">
+     md:pt-8 pb-4  text-4xl   md:text-4xl text-[#7ab916]">
       Customer Reviews?
     </h2>
      {/*Testimonials Container*/} 
     <div className="flex flex-col mt-24 md:flex-row md:gap-12 md:pt-12">
-      {/*Testimonial a*/}
-      <div className="flex flex-col items-center justify-center p-6 space-y-6 rounded-lg  md:w-1/3">
-        <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
+     
+
+       {/*Testimonial a*/}
+      <div className="flex flex-col items-center pt-0 p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
+        <img src="https://images.pexels.com/photos/6311585/pexels-photo-6311585.jpeg" 
+        className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
+        <h5 className="text-lg font-bold text-[#ff7b00]">Ali Hussein</h5>
+        <p className="text-md text-[#7e6b6b] pb-8 font-semibold">
+          “We have been able to cancel so many other subscriptions since
+          using CVV Shop. There is no more cross-channel confusion and
+          everyone is much more focused.”
+        </p>
+      </div>
+
+       {/*Testimonial a*/}
+       <div className="flex flex-col items-center justify-center md:pt-0 p-6 space-y-6 rounded-lg  md:w-1/3">
+        <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg" className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
         <h5 className="text-lg font-bold text-[#ff7b00]">Ann Kandie</h5>
-        <p className="text-md  text-gray-400">
+        <p className="text-md  text-[#7e6b6b] pb-12 font-semibold">
         “CVV Shop is the ultimate high tech modern shop. The ability to make the world a little place
           in terms of the currency access status is what is the most amazing thing. Would recommend 
           anyone all day”
@@ -26,22 +40,11 @@ const Testimonials = () => {
         </p> 
       </div>
 
-       {/*Testimonial a*/}
-      <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
-        <img src="https://images.pexels.com/photos/6311585/pexels-photo-6311585.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
-        <h5 className="text-lg font-bold text-[#ff7b00]">Ali Hussein</h5>
-        <p className="text-md text-gray-400">
-          “We have been able to cancel so many other subscriptions since
-          using CVV Shop. There is no more cross-channel confusion and
-          everyone is much more focused.”
-        </p>
-      </div>
-
        {/*Testimonial c*/}
-      <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
-        <img src="https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
+      <div className="flex flex-col items-center pt-0 p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
+        <img src="https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg" className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
         <h5 className="text-lg font-bold text-[#ff7b00]">Richard Martin</h5>
-        <p className="text-md text-gray-400">
+        <p className="text-md text-[#7e6b6b] pb-8 font-semibold">
         “CVV Shop has supercharged my life. Right now i own a few businesses in town
           and i&apos;m driving my first car”
          
@@ -49,10 +52,10 @@ const Testimonials = () => {
       </div>
     </div>
     
-    <div className="my-16 p-12 hover:animate-bounce">
+    <div className="my-8 p-4 animate-bounce">
       <a
         href="#home"
-        className="p-3 px-6 text-white bg-[#ff3f19] rounded font-semibold
+        className="p-3 px-6 text-white bg-[#16B97A] rounded font-semibold
          no-underline hover:bg-[#b7db37]"
         >Get Started</a >
     </div>
