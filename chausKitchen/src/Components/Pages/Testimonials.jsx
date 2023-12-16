e6289c2485bdaa0aa00581e666edd4ce107556c9
kincaid <kiprotichkincaid@gmail.com>
2023-12-16 15:28:05 +0300

    updates to the codes

--- chausKitchen/src/Components/Pages/Testimonials.jsx
+++ chausKitchen/src/Components/Pages/Testimonials.jsx
@@ -20,7 +20,7 @@ const Testimonials = () => {
       <div className="flex flex-col items-center pt-0 p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
         <img src="https://images.pexels.com/photos/6311585/pexels-photo-6311585.jpeg" 
         className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
-        <h5 className="text-lg font-bold text-[#ff7b00]">Ali Hussein</h5>
+        <h5 className="text-lg font-bold text-[#ff7b00]">Ali Mohammed</h5>
         <p className="text-md text-[#7e6b6b] pb-8 font-semibold">
         “Chaus BBQ is a hidden treasure in Mombasa! The beef ribs are succulent,
          and the secret sauce adds a unique kick.
@@ -31,7 +31,7 @@ const Testimonials = () => {
        {/*Testimonial a*/}
        <div className="flex flex-col items-center justify-center md:pt-0 p-6 space-y-6 rounded-lg  md:w-1/3">
         <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg" className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
-        <h5 className="text-lg font-bold text-[#ff7b00]">Ann Kandie</h5>
+        <h5 className="text-lg font-bold text-[#ff7b00]">Anna Brenda</h5>
         <p className="text-md  text-[#7e6b6b] pb-12 font-semibold">
         “I stumbled upon Chaus BBQ during my visit to Mombasa, and I&apos;m so glad I did! The ambiance is relaxed, the service is excellent,
          and the lamb chops are a taste of heaven. An absolute gem in the heart of Mombasa”
@@ -42,7 +42,7 @@ const Testimonials = () => {
        {/*Testimonial c*/}
       <div className="flex flex-col items-center pt-0 p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
         <img src="https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg" className="-mt-14 w-20 h-20 rounded-full object-cover" alt="" />
-        <h5 className="text-lg font-bold text-[#ff7b00]">Richard Martin</h5>
+        <h5 className="text-lg font-bold text-[#ff7b00]">Lloyd Austin</h5>
         <p className="text-md text-[#7e6b6b] pb-8 font-semibold">
         “Chaus BBQ delivers the authentic taste of BBQ with a coastal twist. The chicken skewers are a flavor explosion, and the samak roasts bring
          out the best of local seafood. Five stars for a memorable dining experience!”
